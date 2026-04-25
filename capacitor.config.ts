ad70a43eac1f4559b7aac58aad8a296b5c37f0ec
MiguelDev1 <loormiguel40@gmail.com>
2026-04-25 08:14:04 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Agronova_Frontend',
+  webDir: 'www'
+};
+
+export default config;
